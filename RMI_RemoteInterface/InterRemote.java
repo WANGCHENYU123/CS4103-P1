c73161a315a9e4076694f284c79308d4b450e3f2
WANGCHENYU123 <541959003@qq.com>
2019-08-31 09:45:38 +0100

    Add files via upload

--- RMI_RemoteInterface/InterRemote.java
+++ RMI_RemoteInterface/InterRemote.java
@@ -1,1 +1,13 @@
+package RMI_RemoteInterface;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import RMI_Client.ClientInterface;
+
+public interface InterRemote extends Remote {
+    public String login(ClientInterface c) throws RemoteException;
+//    public void getInput(String args) throws java.rmi.RemoteException,java.io.IOException;
+//    public String toClient(String b) throws java.rmi.RemoteException,java.io.IOException;
+    public void judgeDecision(String s, ClientInterface c) throws RemoteException; 
+}
